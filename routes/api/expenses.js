c90e5c3ddb7d7f5a34c00b47718996b3e94ca821
Joe Dodgson <j.dodgson@live.co.uk>
2021-12-15 21:19:49 +0000

    Handle get request for a single expense (based on id)

--- routes/api/expenses.js
+++ routes/api/expenses.js
@@ -10,4 +10,19 @@ router.get('/', (req, res) => {
     res.json(expenses);
 });
 
+// Get single expense
+router.get('/:id', (req, res) => {
+    // Obtain expense id from url parameter
+    const expenseId = parseInt(req.params.id);
+
+    // Check if an expense with the provided id exists
+    const found = expenses.some(expense => expense.id === expenseId);
+
+    if (found) {
+        res.json((expenses.filter(expense => expense.id === expenseId)));
+    } else {
+        res.status(400).json({ messge: `No expense with the id of ${expenseId}` });
+    }
+});
+
 module.exports = router;
